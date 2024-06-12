8d21c9aa0c80771ea60d0bd9e009677c37f278f7
Sanjaya Gayan <k.m.s.g.rathnayaka@gmail.com>
2024-06-12 19:53:18 +0530

    Add a about component

---
+++ src/app/about/page.jsx
@@ -0,0 +1,26 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4 pt-4 pb-48">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to Moviehub, your ultimate movie destination! Explore our
+        constantly updated database featuring films from around the world, from
+        Hollywood blockbusters to indie gems. Easily search by title, director,
+        actor, genre, or release date to find the perfect movie for any
+        occasion.
+      </p>
+
+      <p>
+        Stay informed with the latest news, insightful reviews, and exciting
+        trailers. Join our community of movie lovers and dive into the magical
+        world of cinema with Moviehub.Let's embark on this cinematic
+        journey together!
+      </p>
+      
+      <p>
+        Let's embark on this cinematic
+        journey together!
+      </p>
+    </div>
+  );
+}
